e1f2dcd4a261891fb25446b6c2b994a54278dec8
guileful <81571770+Guileful666@users.noreply.github.com>
2021-04-11 21:40:54 +0900

    Add files via upload

---
+++ 02-Value/src/Main01.java
@@ -0,0 +1,39 @@
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		// Value
+		/**
+		 * 첫 라인은 두개 쓰는게 기본
+		 * 
+		 */
+		//변수의 선언
+		int num1;
+		//변수의 할당
+		num1 = 100;
+		
+		//끝에 나타내는 것은 대문자가 좋다
+		//변수 선언 + 할당 + 접미사의 사용
+		long num2 = 123456789000000000L;
+		double num3 = 128.4D;
+		float num4 = 3014F;
+		//다른 자료형
+		byte t = 100;
+		short s = 200;
+		char c = '가';
+		boolean b = true;
+		//문자열과 변수를 더하면, 내용이 연결되어 출력된다. 
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+		System.out.println("num3 = " + num3);
+		System.out.println("num4 = " + num4);
+		
+		System.out.println("t = " + t);
+		System.out.println("s = " + s);
+		System.out.println("c = " + c);
+		System.out.println("b = " + b);
+		
+
+	}
+
+}
