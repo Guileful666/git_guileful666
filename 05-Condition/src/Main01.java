4b89711c5f9e0d5ae29b3f9641d9ff34f0d85ab3
guileful <81571770+Guileful666@users.noreply.github.com>
2021-04-11 21:46:18 +0900

    Add files via upload

---
+++ 05-Condition/src/Main01.java
@@ -0,0 +1,24 @@
+
+public class Main01 {
+
+	public static void main(String[] args) {
+
+		int age = 28;
+		
+		if (age == 28) {
+			System.out.println("28살입니다.");
+		}
+		if (age != 19) {
+			System.out.println("19살이 아닙니다.");
+		}
+		if (age > 19) {
+			System.out.println("성인입니다.");
+		}
+		if (age <= 19) {
+			System.out.println("미성년자입니다.");
+		}
+
+
+	}
+
+}
---
+++ 05-Condition/src/Main04.java
@@ -0,0 +1,24 @@
+
+public class Main04 {
+
+	public static void main(String[] args) {
+		int point = 97;
+		
+		if(90 < point && point <= 100) {
+			System.out.println("A");
+		}
+		else if(80 < point && point <= 90) {
+			System.out.println("B");
+		}
+
+		else if(70 < point && point <= 80) {
+			System.out.println("c");
+		}
+		else {
+			System.out.println("F");
+		}
+
+
+	}
+
+}
